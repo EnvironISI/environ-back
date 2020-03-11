dc289aebe8b46ac691b353337430667292ba1db7
Globalclima <jlucasbrito1@gmail.com>
2020-03-11 21:07:37 +0000

    auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -37,7 +37,6 @@ exports.login = function(req, res, err){
 
   const idToken = req.body.idToken
   const csrfToken = req.body.csrfToken
-  console.log(csrfToken)
   // Guard against CSRF attacks.
   if (csrfToken !== req.cookies.csrfToken) {
     res.status(401).send('UNAUTHORIZED REQUEST!');
@@ -52,18 +51,9 @@ exports.login = function(req, res, err){
   admin.auth().createSessionCookie(idToken, {expiresIn})
     .then((sessionCookie) => {
      // Set cookie policy for session cookie.
-     const options = {
-        secret: 'webbookfca',
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-          secure: true,
-          maxAge: 60000,
-          httpOnly: false,
-        }
-    };
-     res.cookie('session', sessionCookie, options);
-     res.end(JSON.stringify({status: 'success'}));
+    const options = {maxAge: expiresIn, httpOnly: false, secure: true};
+    res.cookie('session', sessionCookie, options);
+    res.end(JSON.stringify({status: 'success'}));
     }, error => {
      res.status(401).send('UNAUTHORIZED REQUEST!');
     });
