58ef0e081be62b9674c42c3c8d04b0ae0f1cef4d
EnvironISI <environisi@gmail.com>
2020-03-29 03:49:45 +0100

    atualizacao

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -332,7 +332,7 @@ exports.camaraEvents = function(req, res, err){
                     qty: 1,
                     offset: 0
                 }
-                moloni.products('getBySearch', params, function(error, result){
+                moloni.products('countBySearch', params, function(error, result){
                     if(error){
                         console.log(error);
                         res.status(400).send(error);
