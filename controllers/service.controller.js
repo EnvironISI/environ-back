85141aa622fb636533268b4eebf88b1e2a92dd0e
EnvironISI <environisi@gmail.com>
2020-03-29 03:41:28 +0100

    atualizacao

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -322,12 +322,7 @@ exports.camaraEvents = function(req, res, err){
                 var params = {
                     company_id: company_id,
                     search: {
-                        properties: [
-                            {
-                                property_id: 11640,
-                                value: user.displayName
-                            }
-                        ]
+                        reference: municipio
                     }
                 }
                 moloni.products('getBySearch', params, function(error, result){
