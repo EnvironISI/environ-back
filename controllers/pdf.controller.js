f3a7359b44b18688e533f6b8dd90d3dc2366d238
EnvironISI <environisi@gmail.com>
2020-05-06 16:58:10 +0100

    atualizacao

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -1,13 +1,15 @@
 const mustache = require("mustache");
 const htmlPdf = require("html-pdf");
-const authDocument = require("../templates/authorization/auth_document");
+const authDocument = require("../templates/auth_document");
+const fs = require("fs");
 
 var exports = module.exports = {};
 
 exports.handlePdf = function (req, res, err) {
-    const content = mustache.render(authDocument.templateStructure, authDocument.templateData);
+    //const content = mustache.render(authDocument.templateStructure, authDocument.templateData);
+    var html = fs.readFileSync('../templates/authorization/auth.html', 'utf8');
     var options = { format: 'Letter' };
-    htmlPdf.create(content, options).toBuffer(function(err, rest) {
+    htmlPdf.create(html, options).toBuffer(function(err, rest) {
         var filename = 'testfile-test';
         filename = encodeURIComponent(filename) + '.pdf'
         res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
