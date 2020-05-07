d9eb5b2d71b69e5a178413513b2536df227d4107
EnvironISI <environisi@gmail.com>
2020-05-07 17:21:09 +0100

    atualizaçao

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -198,7 +198,7 @@ exports.adminAccept = function (req, res, err) {
                             value: result.properties[9].value
                         },
                         {
-                            product_id: 12850,
+                            property_id: 12850,
                             value: result.properties[10].value
                         }
                     ],
@@ -296,7 +296,7 @@ exports.camaraAccept = function (req, res, err) {
                             value: result.properties[9].value
                         },
                         {
-                            product_id: 12850,
+                            property_id: 12850,
                             value: result.properties[10].value
                         }
                     ],
