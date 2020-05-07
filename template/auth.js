b624092effb919c5b413533529609230a506b689
EnvironISI <environisi@gmail.com>
2020-05-07 01:59:24 +0100

    atualizacao

--- template/auth.js
+++ template/auth.js
@@ -6,8 +6,7 @@ var template = `
         <link href="https://environ-back.herokuapp.com/pdf/img/favicon.png" rel="shortcut icon"/>
         <meta charset="utf-8"/>
         <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport"/>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-        <link href="https://environ-back.herokuapp.com/pdf/css/auth.css" rel="stylesheet" type="text/css"/>
+        <link href="https://environ-back.herokuapp.com/pdf/css/invoiceTemplate.css" rel="stylesheet" type="text/css"/>
         <meta content="AnimaApp.com - Design to code, Automated." name="author"/>
     </head>
     <body style="margin: 0;
