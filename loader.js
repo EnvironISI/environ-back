5cbe44924114736f2c025061e2e89d14ac5a86d2
EnvironISI <environisi@gmail.com>
2020-06-29 15:12:17 +0100

    Server

--- loader.js
+++ loader.js
@@ -11,12 +11,13 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const fileUpload = require('express-fileupload');
 
+
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 app.use(cookieParser());
 app.use(fileUpload({
-  useTempFiles : true,
-  tempFileDir : '/tmp/'
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
 }));
 app.set('trust proxy', 1);
 app.use(session({
@@ -30,7 +31,7 @@ app.use(session({
   }
 }));
 app.use(expressValidator());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // check if session exists
   if (global.sessData === undefined) {
     global.sessData = req.session;
@@ -50,4 +51,6 @@ app.use('/user', user);
 app.use('/event', event);
 app.use('/package', package);
 app.use('/', router);
+
+
 module.exports = app;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(cors({
   exposedHeaders: ['Location'],
-  credentials: true, 
+  credentials: true,
   origin: ['http://localhost:8080', 'https://environ-front.herokuapp.com', 'http://environ-front.herokuapp.com', 'http://192.168.64.2', 'https://03cb7f2191214f1ab392e47e8fc4af58.vfs.cloud9.eu-central-1.amazonaws.com', 'http://localhost:8000']
 }));
 app.use(logger('dev'));
@@ -15,7 +15,7 @@ app.use('/assets', express.static('assets'));
 app.use('/views', express.static('views'));
 app.use('/pdf', express.static('template'))
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (!err) {
     console.log('Your app is listening on ' + host + ' and port ' + port);
   }
